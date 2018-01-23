bbae3ff215a2a34b236e92b2b4be6424cf7dbf31
sumit <kumar1993sumit15@gmail.com>
2018-01-23 22:05:50 +0530

    added diff-files method to diff files, line by line, need fixing when no lcs

--- src/linux_commands/diff.clj
+++ src/linux_commands/diff.clj
@@ -1,18 +1,5 @@
 (ns linux-commands.diff
-  ;(:import [clojure.tools.cli :refer [parse-opts]])
-  )
-
-;(def cli-opts
-;  [["-a" "--all"]
-;   ["-r" "--reverse"]
-;   [nil "-m"
-;    :id :fill]
-;   ["-Q" "--quote-name"]
-;   ["-s" "--size"]
-;   ["-l"
-;    :id :long-list]
-;   ["-R" "--recursive"]
-;   ["-p" "--indicator-style"]])
+  (:require [clojure.string :as cstr]))
 
 (defn lcs
   ([x y]
@@ -26,6 +13,39 @@
              b (lcs xs (cons y ys) l)]
          (if (> (count a) (count b)) a b))))))
 
-;(println (lcs
-;           (clojure.string/split "a b c d f g h j q z" #"\s+")
-;           (clojure.string/split "a b c d e f g i j k r x y z" #"\s+")))
+;;From a longest common subsequence it is only a small step to get diff-like output:
+;;
+;; if an item is absent in the subsequence but present in the first original sequence,
+;; it must have been deleted (as indicated by the '-' marks, below).
+;;
+;; If it is absent in the subsequence but present in the second original sequence,
+;; it must have been inserted (as indicated by the '+' marks).
+
+(defn generate-diff
+  ([l1 l2]
+   (let [ls (lcs l1 l2)]
+     (generate-diff ls l1 l2 '())))
+  ([[l & ls] [x & l1] [y & l2] final]
+    (if (nil? l)
+      (reverse final)
+      (cond
+        (and (= l x) (= l y)) (recur ls l1 l2 (cons [nil x] final))
+        (and (some? x) (not= l x)) (recur (cons l ls) l1 (cons y l2) (cons [\- x] final))
+        (and (some? y) (not= l y)) (recur (cons l ls) (cons x l1) l2 (cons [\+ y] final))))))
+
+(defn print-diff
+  [inp]
+  (doseq [[x strr] inp]
+    (if (nil? x)
+      (println "     " strr)
+      (println x "   " strr))))
+
+(defn diff-files
+  [file1 file2]
+  (let [t1 (-> file1 slurp (cstr/split-lines))
+        t2 (-> file2 slurp (cstr/split-lines))]
+    (generate-diff t1 t2)))
+
+;(let [a (cstr/split "a b c d f g h j q z" #"\s+")
+;      b (cstr/split "a b c d e f g i j k r x y z" #"\s+")]
+;  (println (lcs a b)))
